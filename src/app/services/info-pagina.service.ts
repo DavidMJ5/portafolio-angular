876b5833feb4311e5d619a70be17b311528ecc5f
David MJ <davidmj16@hotmail.com>
2018-12-25 19:41:23 +0100

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,16 +9,28 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient ) { 
-    console.log('Servicio de infoPagina cargado');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
 
     // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
-        .subscribe( (resp: InfoPagina) => {
-          this.cargada = true;
-          this.info = resp;
-          console.log(resp);
-        });
+    .subscribe( (resp: InfoPagina) => {
+      this.cargada = true;
+      this.info = resp;
+    });
+  }
+
+  private cargarEquipo(){
+    // Leer el archivo JSON
+    this.http.get('https://angular-html-be6ec.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+      this.equipo = resp;
+    });
   }
 }
