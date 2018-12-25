f76551d666c1583e5df9f6e24dfe903659565ffc
David MJ <davidmj16@hotmail.com>
2018-12-25 21:29:22 +0100

    Fin sección 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -10,16 +10,51 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) { 
     this.cargarProductos();  
   }
 
   private cargarProductos(){
-    this.http.get('https://angular-html-be6ec.firebaseio.com/productos_idx.json')
+
+    return new Promise( ( resolve, reject ) => {
+      this.http.get('https://angular-html-be6ec.firebaseio.com/productos_idx.json')
         .subscribe( (resp: Producto[]) => {
           this.productos = resp;
-          this.cargando = false; 
+          this.cargando = false;
+          resolve();
         });
-  }  
+    }); 
+  }
+
+  getProducto( id: string ){
+    return this.http.get(`https://angular-html-be6ec.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto( termino: string ){
+    if ( this.productos.length === 0){
+      //Cargar productos
+      this.cargarProductos().then( ()=> {
+        //Aplicar el filtro
+        this.filtrarProductos( termino );
+      });
+    }
+    else
+    {
+      //Aplicar el filtro
+      this.filtrarProductos( termino );
+    }
+  }
+
+  private filtrarProductos( termino: string ) {
+    this.productosFiltrado = [];
+    termino = termino.toLocaleLowerCase();
+    this.productos.forEach( prod => {
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+      if ( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0  ) {
+        this.productosFiltrado.push( prod );
+      }
+    });
+  }
 }
